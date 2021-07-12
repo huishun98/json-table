c59b6cd4025bed45d14f2064b13117852f899de0
huishun98 <huishun98@gmail.com>
2021-07-12 12:41:05 +0800

    added vue analytics

--- src/main.js
+++ src/main.js
@@ -7,6 +7,19 @@ import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// import VueGtag from "vue-gtag";
+import VueAnalytics from 'vue-analytics';
+
+// Vue.use(VueGtag, {
+//   config: { id: "UA-201887797-1" }
+// });
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: 'UA-201887797-1',
+  router
+});
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 
